54822b9c547ddf2bff7c5788a19871b3434163ce
Scott Jones <scottdj92@gmail.com>
2023-04-23 14:26:41 -0400

    some additional routing

--- main/apps/server/index.ts
+++ main/apps/server/index.ts
@@ -1,19 +1,49 @@
-import express from "express";
+import express, { Handler } from "express";
 import { MongoClient } from "mongodb";
 
 const PORT = 3000;
 const app = express();
 
-//connect to database
-// set up saving astrology signs per customer
+
 // set up some authentication?
 
+const mongoMiddleware: Handler = async (req, res, next) => {
+    const client = new MongoClient("mongodb://localhost:27017/local");
+
+    await client.connect();
+
+    // @ts-ignore
+    req.db = client.db();
+
+    next();
+};
+
+app.use(mongoMiddleware);
+
 // start server
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
     res.statusCode = 200;
-    res.send("Hello world");
+    // @ts-ignore - need to fix types
+    const data = await req.db.collection("local").find().toArray();
+    res.send(JSON.stringify(data));
+});
+
+app.post("/save", async (req, res) => {
+    // validate input
+
+    // throw error if input does not pass validation
+
+    // otherwise, save to db
+});
+
+app.get("/:id", async (req, res) => {
+    // authenticate user? - doesn't seem necessary
+
+    // fetch from database by id - should come in from the client route
+
+    // const doc = await req.db.findOne({ _id: req.id })
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`);
+    console.log(`Server running on localhost:${PORT}`);
 });
